dddde7169e913a0073111a5dcce3f66b4b4fb57a
tonghai.he@szzjcx.com <hthhit0@gmail.com>
2019-07-19 15:19:59 +0800

    测试消息

--- miniprogram/pages/api-test/test-message/test-message.js
+++ miniprogram/pages/api-test/test-message/test-message.js
@@ -1,1 +1,16 @@
-Page({})
+Page({
+    data: {
+        loading: ''
+    },
+    sender: function () {
+        console.log('click sender ...')
+        this.setData({loading: 'loading'})
+
+
+
+        const that = this;
+        setTimeout(function () {
+            that.setData({loading: ''})
+        }, 3000);
+    }
+})
